94a33329ac6a467a210a71663c00d0d6f489e7d5
Alex Lapinski <contact@alexlapinski.name>
2019-04-22 21:47:22 -0400

    Example create vm

--- infrastructure/project.tf
+++ infrastructure/project.tf
@@ -3,3 +3,29 @@ provider "google" {
     project = "gmail-parser-238501"
     region = "us-east4"
 }
+
+resource "random_id" "instance_id" {
+    byte_length = 8
+}
+
+resource "google_compute_instance" "default" {
+    name = "test-vm-${random_id.instance_id.hex}"
+    machine_type = "f1-micro"
+    zone = "us-east4-a"
+
+    boot_disk {
+        initialize_params {
+            image = "debian-cloud/debian-9"
+        }
+    }
+
+    metadata_startup_script = "sudo apt-get update"
+
+    network_interface {
+        network = "default"
+
+        access_config {
+
+        }
+    }
+}

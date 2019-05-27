89e5ca8de8a2efe40346c6e7de5bdde696abf43a
Alex Lapinski <contact@alexlapinski.name>
2019-05-27 00:25:35 -0400

    Added some tests

--- packages/gmail-parser.core/test/generate-token.main.test.js
+++ packages/gmail-parser.core/test/generate-token.main.test.js
@@ -1,1 +1,28 @@
+const generateToken = require('../generate-token.main');
+
+describe('getCredentials()', () => {
+    describe('when given undefined input', () => {
+        it('should return undefined', () => {
+            const output = generateToken.getCredentialsPath(undefined);
+            expect(output).toBe(undefined);
+        });
+    });
+
+    describe('when given malformed input', () => {
+        it('should return undefined', () => {
+            const input = { 'wrong-prop': 'aValue' };
+            const output = generateToken.getCredentialsPath(input);
+            expect(output).toBe(undefined);
+        });
+    });
+
+    describe('when given valid input', () => {
+        it('should return valid output', () => {
+            const input = { 'credentials_path': 'aValue' };
+            const output = generateToken.getCredentialsPath(input);
+            expect(output).toBe('aValue');
+        });
+    });
+});
+
 module.exports = {};

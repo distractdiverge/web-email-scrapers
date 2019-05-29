e76b5e3a223539a283453fd87c7572385be3a85a
Alex Lapinski <contact@alexlapinski.name>
2019-05-28 21:10:14 -0400

    Added tests for cloud-function

---
+++ packages/gmail-parser.cloud-function/__tests__/index.js
@@ -0,0 +1,67 @@
+const {
+    getMessageData,
+    decodeFromBase64,
+    subscribe,
+} = require('../index');
+
+describe('Cloud Function Entrypoint', () => {
+
+    describe('getMessageData()', () => {
+
+        describe('when given valid input', () => {
+            it('should return the expected value', () => {
+                const input = { 'data': 'aValue' };
+                const output =  getMessageData(input);
+
+                expect(output).toBe('aValue');
+            });
+        });
+
+        describe('when given undefined input', () => {
+            it('should return undefined', () => {
+                const output = getMessageData(undefined);
+                expect(output).toBe(undefined);
+            });
+        });
+    });
+
+    describe('decodeFromBase64()', () => {
+        describe('when given undefined', () => {
+            it('should return undefined', () => {
+                const output = decodeFromBase64(undefined);
+                expect(output).toBe(undefined);
+            });
+        });
+
+        describe('when given malformed input', () => {
+            it('should return garbage', () => {
+                const input = Buffer.from('notBase64').toString('utf8');
+                const output = decodeFromBase64(input);
+                expect(output).toBe('��AjǺ');
+            });
+        });
+
+        describe('when given valid base64 input', () => {
+            it('should return the decoded input', () => {
+                const input = Buffer('aValue').toString('base64');
+                const output = decodeFromBase64(input);
+                expect(output).toBe('aValue');
+            });
+        });
+    });
+
+    describe('subscribe()', () => {
+        describe('when given a valid event', () => {
+           it('should call the callback', () => {
+
+               const event = { data: 'aValue' };
+               const callback = jest.fn();
+
+               subscribe(event, callback);
+
+               expect(callback).toBeCalled();
+           });
+        });
+    });
+
+});
--- packages/gmail-parser.cloud-function/index.js
+++ packages/gmail-parser.cloud-function/index.js
@@ -1,15 +1,19 @@
 const R = require('ramda');
 const { Buffer } = require('safe-buffer');
 
+const getMessageData = R.prop('data');
+const decodeFromBase64 = input => input
+    ? Buffer.from(input, 'base64').toString()
+    : undefined;
+
 const subscribe = (event, callback) => {
-  const pubsubMessage = R.prop('data', event);
-
-  const message = Buffer.from(pubsubMessage, 'base64').toString();
-  console.log(`Message: ${message}`);
-
-  callback();
+    const message = R.pipe(getMessageData, decodeFromBase64)(event);
+    console.log(`Message: ${message}`);
+    callback();
 };
 
 module.exports = {
+    getMessageData,
+    decodeFromBase64,
     subscribe,
 };
